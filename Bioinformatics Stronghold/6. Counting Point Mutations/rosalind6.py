eee64d75297a40200ccb0ab2d9bd49d5d50cb775
hyunjinCHO <xkdl27@naver.com>
2023-12-30 12:52:38 +0900

    rosalind6

---
+++ Bioinformatics Stronghold/6. Counting Point Mutations/rosalind6.py
@@ -0,0 +1,14 @@
+file_name = input()
+
+f = open(file_name, 'r')
+
+seq1 = f.readline()
+seq2 = f.readline()
+
+count = 0
+
+for i in range(len(seq1)):
+    if seq1[i] != seq2[i]:
+        count += 1
+
+print(count)
