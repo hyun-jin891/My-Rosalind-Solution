aef7bbff37bbf63315f142cb952b9bb95c6795cf
hyunjinCHO <xkdl27@naver.com>
2024-02-09 23:01:34 +0900

    rosalind9_2

---
+++ Bioinformatics Stronghold/9. Finding a Motif in DNA/rosalind9_2.py
@@ -0,0 +1,17 @@
+import re
+
+file_name = input()
+f = open(file_name, 'r')
+
+Seq = f.readline()[:-1]
+Motifs = "(?=" + f.readline()[-1] + ")"
+
+
+
+compiled = re.compile(Motifs)
+
+
+it = compiled.finditer(Seq)
+
+for i in it:
+    print(i.span()[0] + 1, end= " ")
