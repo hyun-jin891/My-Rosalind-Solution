822c4b0142f4f7ed6bfd35e9eb7f853ee19bb269
hyunjinCHO <xkdl27@naver.com>
2024-06-13 22:49:32 +0900

    independent

---
+++ 2. Bioinformatics Stronghold/15. Independent Alleles/rosalind15.py
@@ -0,0 +1,21 @@
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+def combination(n, r):
+    return factorial(n)/(factorial(r) * factorial(n - r))
+
+name = input()
+f = open(name, 'r')
+k, N = list(map(int, f.readline().split()))
+
+prob = 1
+num = 2 ** k
+
+for i in range(N):
+    sub_prob = combination(num, i) * (0.75 ** (num - i)) * (0.25 ** i)
+    prob -= sub_prob
+
+print(prob)
+    
